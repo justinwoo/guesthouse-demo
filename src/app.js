3e803bc6d4d0af32782f833a281469c91ba21f60
Justin Woo <moomoowoo@gmail.com>
2014-09-14 00:54:55 -0400

    More styling work, handling

--- src/app.js
+++ src/app.js
@@ -1,3 +1,37 @@
-var director = require('director');
+var Router = require('director').Router;
+var $ = require('jquery');
 
-console.log('director', director);
+var handleLogin = function () {
+  $('.carousel-heading').text('Login');
+};
+
+var handleRegister = function () {
+  $('.carousel-heading').text('Create your account');
+};
+
+// hide the sections that we aren't using for this path.
+// kind of messy, but this can be redone later with a library.
+var handleBeforeRoute = function () {
+  var pathname = window.location.pathname;
+  var sections = $('section');
+  $('section').each(function (i, ele) {
+    var route = $(ele).data('route');
+    if (pathname.indexOf(route) === -1) {
+      $(ele).hide();
+    }
+  });
+};
+
+routes = {
+  '/login': handleLogin,
+  '/register': handleRegister
+};
+
+router = Router(routes);
+
+router.configure({
+  before: handleBeforeRoute,
+  html5history: true
+});
+
+router.init();
